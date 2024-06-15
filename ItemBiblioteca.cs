b0c0771f9502e74676ed9bbdc3e2ad7a61fffb93
FeHunter <hunterfe1111@gmail.com>
2024-06-15 12:55:00 -0300

    adição de métodos abstratos em ItemBiblioteca.

--- ItemBiblioteca.cs
+++ ItemBiblioteca.cs
@@ -9,5 +9,9 @@ namespace AtCSharp03
         public string Titulo { get => _titulo; set => _titulo = value; }
         public string Autor { get => _autor; set => _autor = value; }
         public int Ano { get => _ano; set => _ano = value; }
+
+        public abstract void Emprestar();
+        public abstract void Devolver();
+        public abstract void ExibirInformacoes();
     }
 }
