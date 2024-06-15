2be089a96399bcf08dd5671cae20cacd0656cb86
FeHunter <hunterfe1111@gmail.com>
2024-06-15 14:23:20 -0300

    Criando livros padrão e menu no programa principal

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace AtCSharp03
     {
         static void Main ()
         {
-            
+
+            // Livros e Revista
+            Livro l1 = new Livro("Vanilla Sky", "Tom Cruise", 2001);
+            Livro l2 = new Livro("O Conde de Monte Cristo", "Edmond Dante", 1983);
+            Revista r1 = new Revista("Revista Monet", "Ellis", 2004);
+            Revista r2 = new Revista("Revista Caras", "Pedro", 2009);
+
+            bool fim = false;
+            while (!fim)
+            {
+                int op = Menu();
+            }
+        }
+
+        static int Menu ()
+        {
+            Console.WriteLine("Sistema da Biblioteca");
+            Console.WriteLine("1 - Lista de livros e revistas");
+            Console.WriteLine("2 - Adicionar Item");
+            Console.WriteLine("3 - Sair");
+            int op = 0;
+            do{
+                op = int.Parse(Console.ReadLine());
+            }while (op < 1 || op > 3);
+            return op;
         }
     }
 }
