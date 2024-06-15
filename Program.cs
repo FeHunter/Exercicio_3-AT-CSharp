08d0bbbcf7a76cfdd4eaae747a2e450332b36d23
FeHunter <hunterfe1111@gmail.com>
2024-06-15 14:31:45 -0300

    Adicionar livros padrões na biblioteca

--- Program.cs
+++ Program.cs
@@ -7,23 +7,40 @@ namespace AtCSharp03
         static void Main ()
         {
 
-            // Livros e Revista
-            Livro l1 = new Livro("Vanilla Sky", "Tom Cruise", 2001);
-            Livro l2 = new Livro("O Conde de Monte Cristo", "Edmond Dante", 1983);
-            Revista r1 = new Revista("Revista Monet", "Ellis", 2004);
-            Revista r2 = new Revista("Revista Caras", "Pedro", 2009);
+            Biblioteca biblioteca = new Biblioteca();
+            AdicionarLivrosRevistas(biblioteca);
 
             bool fim = false;
             while (!fim)
             {
                 int op = Menu();
+
+                if (op == 1)
+                {
+
+                }
+            }
+        }
+
+        static void AdicionarLivrosRevistas (Biblioteca biblioteca)
+        {
+            // Coloar livros e revistas na biblioteca "Já cadastrados".
+            List<ItemBiblioteca> itens = new List<ItemBiblioteca>() {
+                new Livro("Vanilla Sky", "Tom Cruise", 2001),
+                new Livro("O Conde de Monte Cristo", "Edmond Dante", 1983),
+                new Revista("Revista Monet", "Ellis", 2004),
+                new Revista("Revista Caras", "Pedro", 2009)
+            };
+            foreach (ItemBiblioteca item in itens)
+            {
+                biblioteca.AdicionarItem(item);
             }
         }
 
         static int Menu ()
         {
             Console.WriteLine("Sistema da Biblioteca");
-            Console.WriteLine("1 - Lista de livros e revistas");
+            Console.WriteLine("1 - Exibir livros e revistas");
             Console.WriteLine("2 - Adicionar Item");
             Console.WriteLine("3 - Sair");
             int op = 0;
