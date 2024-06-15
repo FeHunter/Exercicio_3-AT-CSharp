46f99ecb7f4dbe5f664bd6a68d72954791885ba0
FeHunter <hunterfe1111@gmail.com>
2024-06-15 12:52:59 -0300

    Criada classe abstrata ItemBiblioteca

---
+++ ItemBiblioteca.cs
@@ -0,0 +1,13 @@
+namespace AtCSharp03
+{
+    abstract class ItemBiblioteca
+    {
+        private string _titulo;
+        private string _autor;
+        private int _ano;
+
+        public string Titulo { get => _titulo; set => _titulo = value; }
+        public string Autor { get => _autor; set => _autor = value; }
+        public int Ano { get => _ano; set => _ano = value; }
+    }
+}
